885d6aa37dbdfc00bbe91aa61c2c7eeeb8923965
Cielo Muyot <cielo.muyot@pulseid.com>
2022-07-19 18:17:33 +0800

    Documentation: Fixed missing definitions

--- swagger.js
+++ swagger.js
@@ -2,7 +2,6 @@ const swaggerJSDoc = require('swagger-jsdoc');
 const config = require('config');
 
 const swaggerDefinition = {
-  openapi: '3.0.0',
   info: {
     version: '1.0.0',
     title: 'Xendit tech assessment - Cielo Muyot'
